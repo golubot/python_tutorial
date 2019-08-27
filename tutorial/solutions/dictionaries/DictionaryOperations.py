fd79ddc6c46fb07e4056cf25b40fb5c82f0865e6
golubot <agolubovski@gmail.com>
2019-08-27 10:18:05 +0200

    - added .idea to .gitignore - added dictionary tests

---
+++ tutorial/solutions/dictionaries/DictionaryOperations.py
@@ -0,0 +1,11 @@
+def add_elements_if_number_of_elements_is_odd(in_dict, element):
+    """
+    Adds dictionary element {'key': value} if number of current elements in the input dictionary is odd.
+    DO NOT REPLACE THE EXISTING VALUE IF THE ELEMENT IS ALREADY IN THE DICTIONARY
+
+    :param in_dict (dict): Input dictionary which will possibly hold the element passed as a second parameter
+    :param element (dict): Element for potential addition
+    :return (Boolean, int): Tuple with first parameter True if added False otherwise, and second - number of elements added
+    """
+
+    return False, -1
