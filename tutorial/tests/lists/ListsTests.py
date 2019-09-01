f67802cf91c1d46537a4e41e8fbd92bde7852f1c
Aleksandar Golubovski <agolubovski@gmail.com>
2019-09-01 21:57:50 +0200

    - added implementation  + tests

--- tutorial/tests/lists/ListsTests.py
+++ tutorial/tests/lists/ListsTests.py
@@ -1,7 +1,7 @@
 import unittest
 from tutorial.solutions.lists.ListOperations import concatenate_lists, concatenate_list_with_reverse_second_list, \
     concatenate_and_sort, extract_even_elements, delete_even_elements, extract_every_third_element_using_slices, \
-    delete_element_from_list_if_present
+    delete_element_from_list_if_present, concatenate_reversed_input_lists, switch_elements_by_index
 
 
 class ListConcatenationTestCase(unittest.TestCase):
@@ -31,7 +31,6 @@ class ListConcatenationTestCase(unittest.TestCase):
 
         self.assertIsNotNone(result)
         self.assertEquals(["d", "r", "y", "t"], result)
-        # self.assertEqual(True, False)
 
     def test_list_concatenation_with_empty_first_list(self):
         """
@@ -83,7 +82,10 @@ class ListConcatenationTestCase(unittest.TestCase):
 
         first_list = [1, 2, 3, 4]
         second_list = [11, 22, 33]
-        self.assertEqual(True, False)
+
+        result = concatenate_reversed_input_lists(first_list, second_list)
+
+        self.assertEquals([4, 3, 2, 1, 33, 22, 11], result)
 
     def test_sorted_list(self):
         """
@@ -94,28 +96,33 @@ class ListConcatenationTestCase(unittest.TestCase):
         second_list = [11, 22, 33]
         result = concatenate_and_sort(first_list, second_list)
 
-        # Add result assertions here
+        self.assertIsNotNone(result)
+        self.assertEquals([1, 2, 3, 4, 11, 22, 33], result)
 
         first_list = [ "a", "b", "c"]
         second_list = ["x", "i", "p"]
         result = concatenate_and_sort(first_list, second_list)
 
-        # Add result assertions here
+        self.assertEquals(["a", "b", "c", "i", "p", "x"], result)
 
         first_list = [ "banana", "curiosity", "ana"]
         second_list = ["killed", "the", "cat"]
         result = concatenate_and_sort(first_list, second_list)
 
-        # Add result assertions here
-
-        self.assertEqual(True, False)
+        self.assertEquals(["ana", "banana", "cat", "curiosity", "killed", "the"], result)
 
     def test_switch_elements_by_index(self):
         """
         Write test and implementation of substitution of elements in two list. Eg:
         For [1, 2, 3] [4, 5, 6] substitute(2) returns [1, 2, 6] [4, 5, 3]
         """
-        self.assertEqual(True, False)
+        first_list = [1, 3, 5]
+        second_list = [2, 4, 6, 8]
+
+        result1, result2 = switch_elements_by_index(first_list, second_list, 2)
+
+        self.assertEquals([1, 3, 6], result1)
+        self.assertEquals([2, 4, 5, 8], result2)
 
     def test_extract_even_elements(self):
         seq = range(0, 100)
@@ -133,15 +140,18 @@ class ListConcatenationTestCase(unittest.TestCase):
         input_list = list(seq)
 
         result = delete_even_elements(input_list)
+        print(result)
         self.assertEqual(50, len(result))
         self.assertEqual(50, len(input_list))
         self.assertEqual(input_list, result)
 
     def test_extract_every_third_element(self):
         result = extract_every_third_element_using_slices(list(range(0, 100)))
+        print(result)
         self.assertEqual(result, list(range(3, 100, 3)))
 
         result = extract_every_third_element_using_slices(list(range(0, 1)))
+        print(result)
         self.assertEqual(result, list())
 
         result = extract_every_third_element_using_slices(list(range(0, 2)))
