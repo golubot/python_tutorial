a12fa25329290449e53d898b0c6bd0ab3c41ce42
Aleksandar Golubovski <agolubovski@gmail.com>
2019-09-03 23:01:42 +0200

    - added solution for Dog class

--- tutorial/solutions/oop/Dog.py
+++ tutorial/solutions/oop/Dog.py
@@ -1,1 +1,17 @@
 class Dog():
+
+    #class attributes
+    species = "mammal"
+
+    #instance attributes
+    def __init__(self, breed, name, spots):
+        self.breed = breed
+        self.name = name
+        self.spots = spots
+
+    def get_name(self):
+        return(self.name)
+
+
+
+
