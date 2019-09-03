9408408dbec0ccc13e92aa9d154eae7b0c88b154
Aleksandar Golubovski <agolubovski@gmail.com>
2019-09-04 00:13:08 +0200

    - added solution for Animal class, Dog class with inheritance - added new tests

--- tutorial/solutions/oop/Dog.py
+++ tutorial/solutions/oop/Dog.py
@@ -1,10 +1,14 @@
-class Dog():
+from tutorial.solutions.oop.Animal import Animal
+
+
+class Dog(Animal):
 
     #class attributes
     species = "mammal"
 
     #instance attributes
     def __init__(self, breed, name, spots):
+        super().__init__()
         self.breed = breed
         self.name = name
         self.spots = spots
@@ -12,6 +16,5 @@ class Dog():
     def get_name(self):
         return(self.name)
 
-
-
-
+    def eat(self):
+        return "I am a dog eating"
--- tutorial/tests/oop/DogsTestCase.py
+++ tutorial/tests/oop/DogsTestCase.py
@@ -1,4 +1,6 @@
 import unittest
+
+from tutorial.solutions.oop.Animal import Animal
 from tutorial.solutions.oop.Dog import Dog
 
 
@@ -14,15 +16,28 @@ class MyTestCase(unittest.TestCase):
         type(my_dalmatian)
         self.assertNotEqual(my_schnauzer, my_dalmatian)
 
-        self.assertTrue("Fido", my_schnauzer.name) # get name attribute
-        self.assertTrue("Fido", my_schnauzer.get_name()) # get name function
-        self.assertTrue("Chejs", my_dalmatian.get_name())
-        self.assertTrue("mammal", my_dalmatian.species) # should be class object attribute
-        self.assertTrue("mammal", Dog.species) # another way to access class object attributes
+        self.assertEqual("Fido", my_schnauzer.name) # get name attribute
+        self.assertEqual("Fido", my_schnauzer.get_name()) # get name function
+        self.assertEqual("Chejs", my_dalmatian.get_name())
+        self.assertEqual("mammal", my_dalmatian.species) # should be class object attribute
+        self.assertEqual("mammal", Dog.species) # another way to access class object attributes
 
     # Make Dog inherit Animal and write the same test against Animal. Don't forget to call the super constructor and pass the Dog reference to Animal
+
+    def test_dog_from_animals(self):
     # Test if all Dog objects can eat if created this way
+        my_rottweiler = Dog("Rottweiler", "Butch", False)
+        my_boxer = Dog("Boxer", "Vane", True)
+
+        self.assertEqual("I am a dog eating", my_rottweiler.eat())
+        self.assertEqual("I am a dog eating", my_boxer.eat())
     # Overwrite eating method to return "I am a dog eating"
+
+    def test_animal(self):
+        my_lion = Animal()
+
+        self.assertEqual("I am eating", my_lion.eat())
+        self.assertRaises(NotImplementedError, my_lion.get_name)
 
 if __name__ == '__main__':
     unittest.main()
