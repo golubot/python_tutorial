d8611255e1146e650078a1f81e693e384146f564
golubot <agolubovski@gmail.com>
2019-09-06 10:02:34 +0200

    - added one more pandas task

---
+++ tutorial/solutions/pandas/dataframe_excersizes.py
@@ -0,0 +1,6 @@
+"""
+Follow the examples in this simple tutorial:
+https://data36.com/pandas-tutorial-1-basics-reading-data-files-dataframes-data-selection/
+Write tests and implementations for all outstanding examples there.
+The .py file that holds the test is missing. Create one.
+"""
