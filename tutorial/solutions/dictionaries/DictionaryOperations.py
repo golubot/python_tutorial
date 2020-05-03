8fc1a0f2ebfdb961c3701c679dbf28285f3d66f9
Aleksandar Golubovski <agolubovski@gmail.com>
2020-05-03 14:25:03 +0200

    Implemented solution for Dictionary Opertions

--- tutorial/solutions/dictionaries/DictionaryOperations.py
+++ tutorial/solutions/dictionaries/DictionaryOperations.py
@@ -3,9 +3,23 @@ def add_elements_if_number_of_elements_is_odd(in_dict, element):
     Adds dictionary element {'key': value} if number of current elements in the input dictionary is odd.
     DO NOT REPLACE THE EXISTING VALUE IF THE ELEMENT IS ALREADY IN THE DICTIONARY
 
-    :param in_dict (dict): Input dictionary which will possibly hold the element passed as a second parameter
-    :param element (dict): Element for potential addition
-    :return (Boolean, int): Tuple with first parameter True if added False otherwise, and second - number of elements added
+    :param in_dict: Input dictionary which will possibly hold the element passed as a second parameter
+    :param element: Element for potential addition
+    :return (Boolean, int): Tuple with first parameter True if added False otherwise, and second - number of elements
+    added
     """
 
-    return False, -1
+    is_added = False
+    no_elements = 0
+
+    if not element:
+        return False, 0
+
+    if len(in_dict) % 2 != 0:
+        for key in element.keys():
+            if key not in in_dict:
+                in_dict[key] = element[key]
+                is_added = True
+                no_elements += 1
+
+    return is_added, no_elements
