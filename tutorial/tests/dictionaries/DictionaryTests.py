f2a694edb55aea49003189d3614fb1d6dce32e34
Aleksandar Golubovski <agolubovski@gmail.com>
2020-05-03 14:26:44 +0200

    Implemented tests for   - test_add_elements_on_even_dict   - test_add_empty_dict   - test_add_element_that_exist_in_input_dict   - test_add_elements_that_exist_in_input_dict

--- tutorial/tests/dictionaries/DictionaryTests.py
+++ tutorial/tests/dictionaries/DictionaryTests.py
@@ -15,29 +15,40 @@ class DictionaryTestCase(unittest.TestCase):
 
     def test_add_elements_on_even_dict(self):
         """
-        Write the test only. The subject of the test is to prove that when called add_elements_if_number_of_elements_is_odd()
-        with even number of elements - it does not add it to the input dict.
+        Write the test only. The subject of the test is to prove that when called
+        add_elements_if_number_of_elements_is_odd() with even number of elements - it does not add it to the input dict.
         """
-        self.assertEqual(True, False)
+
+        test_dict = {'name': 'ford', 'make': 'mondeo', 'door_count': 5, "driver": "Alek"}
+        is_added, number_of_added_elements = add_elements_if_number_of_elements_is_odd(test_dict,
+                                                                                       {'engine_displacement': 2000})
+        self.assertEqual(False, is_added)
 
     def test_add_empty_dict(self):
         """
         Write the test only. The subject of the test is to prove that when called with empty dict there is no error
         in the implementation (the program won't brake)
         """
-        self.assertEqual(True, False)
+
+        test_dict = {'name': 'ford', 'make': 'mondeo', 'door_count': 5}
+        is_added, number_of_added_elements = add_elements_if_number_of_elements_is_odd(test_dict, {})
+
+        self.assertFalse(is_added)
+        self.assertEqual(0, number_of_added_elements)
+        self.assertEqual(test_dict, {'name': 'ford', 'make': 'mondeo', 'door_count': 5})
 
     def test_add_element_that_exist_in_input_dict(self):
         """
         Finish writing the test only. The subject of the test is to prove that when called with empty dict there is no
         error in the implementation
         """
-        test_dict = {'name': 'ford', 'make': 'mondeo', 'door_count': 5}
-        add_element = {'engine_displacement': 2000}
-        is_added = True  # delete this
-        self.assertFalse(is_added)
 
-        self.assertEqual(True, False)  # delete this
+        test_dict = {'name': 'ford', 'make': 'mondeo', 'door_count': 5}
+        add_element = {'make': 2000}
+
+        is_added, number_of_added_elements = add_elements_if_number_of_elements_is_odd(test_dict, add_element)
+
+        self.assertFalse(is_added)
 
     def test_add_elements_that_exist_in_input_dict(self):
         """
@@ -48,8 +59,8 @@ class DictionaryTestCase(unittest.TestCase):
         add_element = {'door_count': 5, 'engine_displacement': 2000,
                        'drivers': [{'name': 'john', 'surname': 'doe'}, {'name': 'jane', 'surname': 'doe'}]
                        }
-        is_added = False  # delete this
-        number_of_added_elements = -1  # delete this
+
+        is_added, number_of_added_elements = add_elements_if_number_of_elements_is_odd(test_dict, add_element)
 
         self.assertTrue(is_added)
         self.assertEqual(2, number_of_added_elements)
