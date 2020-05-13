71bbd7a519bc5ea8dfbc37580ca8fa0d76ff5760
Aleksandar Golubovski <agolubovski@gmail.com>
2020-05-13 18:53:10 +0200

    Modified tests and solutons for dictionaries:   - DictionaryTests modified   - DictionaryOperations modified   - corrected value for "publish_event" set to false   - created new .json file

--- tutorial/solutions/dictionaries/DictionaryOperations.py
+++ tutorial/solutions/dictionaries/DictionaryOperations.py
@@ -36,28 +36,67 @@ def publish_event_if_stated_in_header(in_dict):
     :param in_dict: Dictionary for user display, if there is a header flag set to true
     """
     # write conditions
+    if 'header' not in in_dict['payload']:
+        return 'Missing [header] key in dictionary!'
 
-    # hide main window
-    root = tkinter.Tk()
-    root.withdraw()
-    messagebox.showinfo("Hooray, you got an event!", json.dumps(in_dict, indent=4))
+    if 'publish_event' not in in_dict['payload']['header']:
+        return 'Missing [publish_event] key in dictionary!'
+
+    if not in_dict['payload']['header']['publish_event']:
+        return False
+    else:
+        # hide main window
+        root = tkinter.Tk()
+        root.withdraw()
+        messagebox.showinfo("Hooray, you got an event!", json.dumps(in_dict, indent=4))
+        return True
 
 
 def event_from_dictionary_to_csv(in_dict):
     """
     Creates a flat csv representation from the nested input directory with one row as header and one row as data.
-    :param in_dict dict: Input that will be translated to csv format
-    :return: ist(list): List of lists of which the first list is the header, second third and any subsequent list represents data row
+    :param in_dict: Input that will be translated to csv format
+    :return: list(list): List of lists of which the first list is the header, second third and any subsequent list
+    represents data row
     """
-    result = list()
-    result.append(list()) # header
-    result.append(list()) # data
-    return result
+    # header: extract all keys before you append
+
+    key_list = list(in_dict.keys())
+    value_list = list()
+    for key in key_list:
+        value = in_dict[key]
+        if not isinstance(value, dict):
+            if type(value) in (bool, int):
+                value_list.append(str(value))
+            else:
+                value_list.append(value)
+        else:
+            value_list.append('')
+            rec_keylist, rec_valuelist = event_from_dictionary_to_csv(value)
+            key_list = key_list + rec_keylist
+            value_list = value_list + rec_valuelist
+
+    return key_list, value_list
+
+
+def m2(in_dict):
+    keys, values = event_from_dictionary_to_csv(in_dict)
+
+    return [",".join(keys), ",".join(values)]
 
 
 def write_input_csv_to_file(in_csv):
     """
     Writes the csv passed as an argument to this function to a file.
-    :param in_csv list(list()): List of lists of which the first list is the header, second third and any subsequent list represents data row
+    :param in_csv list(list()): List of lists of which the first list is the header, second third and any subsequent
+    list represents data row
     """
+
+    my_file = open("my_first_test.txt", "a")
+    for row in in_csv:
+        my_file.write(row)
+        my_file.write("\n")
+    # my_file.writelines(in_csv)
+    my_file.close()
+
     return
--- tutorial/tests/dictionaries/DictionaryTests.py
+++ tutorial/tests/dictionaries/DictionaryTests.py
@@ -1,12 +1,13 @@
+import json
 import unittest
 
 from tutorial.solutions.dictionaries.DictionaryOperations import add_elements_if_number_of_elements_is_odd, \
-    write_input_csv_to_file, event_from_dictionary_to_csv
+    write_input_csv_to_file, event_from_dictionary_to_csv, publish_event_if_stated_in_header, m2
 
 
 class DictionaryTestCase(unittest.TestCase):
     # global variable, reference it with self.
-    JSON_EVENT_HEADER_MISSING = '''{"dataset_id":"custom_dataset_id","event_id":"0ff28e2d-698f-4e82-919a-ca617be82a03","environment_id":"local","payload":{"payload_version":"3.0","header":{"event_type":"learning","event_timestamp":"2019-07-25T10:18:31.814608Z","event_origin":"test-event-origin","source_id":"test-source-id","source_version":"$LATEST","domain":"loopback"},"data":{"producer":{"actor_type":"local","system_id":"test-system"},"consumer":{"actor_type":"system","system_id":"My system id"},"facts":{"claim_reference":"4002807892","original_id":0,"new_id":37}},"status":{"status_code":"100","status_message":"Success"}},"retention":{"delete":true,"deletion_date":"2068-04-23T18:25:43.511Z","obfuscate":false}}'''
+    JSON_EVENT_HEADER_MISSING = '''{"dataset_id":"custom_dataset_id","event_id":"0ff28e2d-698f-4e82-919a-ca617be82a03","environment_id":"local","payload":{"payload_version":"3.0", "data":{"producer":{"actor_type":"local","system_id":"test-system"},"consumer":{"actor_type":"system","system_id":"My system id"},"facts":{"claim_reference":"4002807892","original_id":0,"new_id":37}},"status":{"status_code":"100","status_message":"Success"}},"retention":{"delete":true,"deletion_date":"2068-04-23T18:25:43.511Z","obfuscate":false}}'''
 
     def test_add_elements_on_odd_dict(self):
         test_dict = {'name': 'ford', 'make': 'mondeo', 'door_count': 5}
@@ -70,23 +71,36 @@ class DictionaryTestCase(unittest.TestCase):
 
     def test_publish_events_happy(self):
         """
-        Tests the happy path of the publishing events. The subject under test is to confirm if the dialog box is shown to the user
+        Tests the happy path of the publishing events. The subject under test is to confirm if the dialog box
+        is shown to the user
         """
         # open file under '../resources/event_positive.json'
         # load it as json
         # call publish_event_if_stated_in_header
         # Assert something
-        self.assertFalse(True)
+
+        with open('../resources/event_positive.json') as positive_event_file:
+            publish_true_dict = json.load(positive_event_file)
+
+        return_value = publish_event_if_stated_in_header(publish_true_dict)
+
+        self.assertEqual(True, return_value)
 
     def test_publish_events_happy_no_publish(self):
         """
-        Tests the happy path of the publishing events. The subject under test is to confirm if the dialog box is NOT shown to the user
+        Tests the happy path of the publishing events. The subject under test is to confirm if the dialog box
+        is NOT shown to the user
         """
         # open file under '../resources/event_negative.json'
         # load it as json
         # call publish_event_if_stated_in_header
         # Assert something
-        self.assertFalse(True)
+        with open('../resources/event_negative.json') as negative_event_file:
+            publish_false_dict = json.load(negative_event_file)
+
+        return_value = publish_event_if_stated_in_header(publish_false_dict)
+
+        self.assertEqual(False, return_value)
 
     def test_publish_events_unhappy_publish_missing(self):
         """
@@ -96,7 +110,12 @@ class DictionaryTestCase(unittest.TestCase):
         # load it as json
         # call publish_event_if_stated_in_header
         # Assert something
-        self.assertFalse(True)
+        with open('../resources/event_publish_missing.json') as publish_missing_file:
+            publish_missing_dict = json.load(publish_missing_file)
+
+        return_value = publish_event_if_stated_in_header(publish_missing_dict)
+
+        self.assertEqual('Missing [publish_event] key in dictionary!', return_value)
 
     def test_publish_events_unhappy_header_missing(self):
         """
@@ -104,18 +123,31 @@ class DictionaryTestCase(unittest.TestCase):
         """
         # Serialize string variable DictionaryTestCase.JSON_EVENT_HEADER_MISSING (or self.JSON_EVENT_HEADER_MISSING)
         # as dictionary call publish_event_if_stated_in_header
+        test_dictionary = json.loads(self.JSON_EVENT_HEADER_MISSING)
+
+        return_value = publish_event_if_stated_in_header(test_dictionary)
 
         # Assert something
-        self.assertFalse(True)
+        self.assertEqual('Missing [header] key in dictionary!', return_value)
 
     def test_dict_to_csv(self):
         """
         Tests the creation of csv from passed input dictionary. Use the experience gained from previous tests to
         load a json file as dictionary and pass it to this method asserting the output created from the file's contents
         """
-        out = event_from_dictionary_to_csv({})
+        with open('../resources/event_positive_alternative.json') as event_alternative_file:
+            input_dict = json.load(event_alternative_file)
+
+        csv_list = m2(input_dict)
         # assert output to be as expected (create expected output as list containing header and data)
-        self.assertTrue(False)
+
+        self.assertEqual(['dataset_id,event_id,environment_id,payload,retention,payload_version,header,data,status,event_type,publish_event,'
+         'event_timestamp,event_origin,source_id,source_version,domain,producer,consumer,facts,actor_type,system_id,actor_type,system_id,'
+         'claim_reference,original_id,new_id,status_code,status_message,delete,deletion_date,obfuscate',
+         'custom_dataset_id,372d37a4-ca3a-11e9-ac8e-e8afe580d403,local,,3.0,,learning 2,True,2019-09-11T04:28:55.814608Z,'
+         'test-event-origin,test-source-id,$LATEST,loopback,,,local,test-system,,system,My system id,,55602835266,0,37,,'
+         '100,Success,,True,2068-04-23T18:25:43.511Z,False'],
+                         csv_list)
 
     def test_write_csv_to_file(self):
         """
@@ -123,14 +155,19 @@ class DictionaryTestCase(unittest.TestCase):
         to create the input (you can use previous function as utility) which will be passed to the create file function.
         """
 
-        csv_struct = list()  # create empty list
-        csv_struct.append(list()) # add empty list as header
-        csv_struct.append(list()) # add empty list as data row 1
-        csv_struct.append(list()) # add empty list as data row 2
+        with open('../resources/event_positive.json') as event_pos_file:
+            input_dict = json.load(event_pos_file)
+
+        csv_struct = m2(input_dict)
 
         write_input_csv_to_file(csv_struct)
+
+        read_text = open("my_first_test.txt", "r")
+        actual = read_text.read()
+
         # assert that the file exists and is populated as expected
-        self.assertFalse(True)
+        self.assertEqual('''dataset_id,event_id,environment_id,payload,retention,payload_version,header,data,status,event_type,publish_event,event_timestamp,event_origin,source_id,source_version,domain,producer,consumer,facts,actor_type,system_id,actor_type,system_id,claim_reference,original_id,new_id,status_code,status_message,delete,deletion_date,obfuscate\ncustom_dataset_id,0ff28e2d-698f-4e82-919a-ca617be82a03,local,,3.0,,learning,True,2019-07-25T10:18:31.814608Z,test-event-origin,test-source-id,$LATEST,loopback,,,local,test-system,,system,My system id,,4002807892,0,37,,100,Success,,True,2068-04-23T18:25:43.511Z,False\n''', actual)
+
 
 if __name__ == '__main__':
     unittest.main()
